37af35546be0f772d8e31e96d8b5074611713e96
madhead <siarhei.krukau@gmail.com>
2015-04-11 01:41:58 +0300

    Do not create any tasks by default

--- src/main/groovy/by/dev/madhead/gbp/GradleBackupPlugin.groovy
+++ src/main/groovy/by/dev/madhead/gbp/GradleBackupPlugin.groovy
@@ -15,7 +15,6 @@
  */
 package by.dev.madhead.gbp
 
-import by.dev.madhead.gbp.tasks.gdrive.ObtainGoogleDriveTokensTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -30,7 +29,6 @@ class GradleBackupPlugin implements Plugin<Project> {
 	@Override
 	void apply(Project project) {
 		project.configure(project) {
-			project.task('obtainGoogleDriveTokens', type: ObtainGoogleDriveTokensTask)
 		}
 	}
 }
